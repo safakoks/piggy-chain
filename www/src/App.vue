4e1172c20cea4a8b1deb8a793143cd87a823c853
safakoks <safakoksuzer@gmail.com>
2020-04-19 17:26:22 +0300

    add all configs

---
+++ www/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <title>Piggy Chain</title>
+    <img alt="Vue logo" src="./assets/piggy.svg">
+    <AddMoneyComp></AddMoneyComp>
+    <SetUserComp></SetUserComp>
+  </div>
+</template>
+
+<script>
+  import AddMoneyComp from './components/AddMoney.vue'
+  import SetUserComp from './components/SetUser.vue'
+
+export default {
+  name: 'App',
+  components: {
+    AddMoneyComp,
+    SetUserComp
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
